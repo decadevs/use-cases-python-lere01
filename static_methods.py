e1ce5dc6d5135a720c1b1cc2904e70ead4f16e2a
Faith Oyedemi <faithoyedemi@yahoo.com>
2020-01-20 15:09:00 +0100

    update static_methods.py script

--- static_methods.py
+++ static_methods.py
@@ -1,1 +1,27 @@
 # Document at least 3 use cases of static methods
+
+"""
+Static methods do not take the 'self' or 'cls' parameters although they can take any number of arguments.
+They cannot modify object or class state except when called by class or instance methods. In this wise, 
+they can be called helper functions which is one way to use them. However, they are primarily a way to 
+namespace methods
+"""
+
+# A class of circles
+class Cirlce:
+    def __init__(self, radius, circumference = 0, area = 0):
+        self.radius = radius
+        self.area = area
+        self.circumference = circumference
+
+    def calculate_area(self):
+        self.area = (2 * math.pi) * (self.radius ** 2)
+        return self.area
+
+    def calculate_circumference(self):
+        self.circumference = (2 * math.pi) * self.calculate_diameter(self.radius)
+        return self.circumference
+        
+    @staticmethod
+    def calculate_diameter(radius):
+        return 2 * radius
